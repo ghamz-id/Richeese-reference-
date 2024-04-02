2ad1a44b73a51cb37e6758d168c5c687e27ab847
ghamz-id <hcoustic@gmail.com>
2024-04-02 11:08:03 +0700

    Layouting - Halaman register: /register

--- my-app/src/app/register/page.tsx
+++ my-app/src/app/register/page.tsx
@@ -17,9 +17,16 @@ export default function Login() {
                 </div>
                 <div className="flex-2 bg-white h-[70%] min-w-[25%] rounded-xl bg-opacity-15 backdrop-blur-sm border border-l-slate-400">
                     <div className="flex flex-col items-center justify-center h-full px-4">
-                        <p className="font-bold text-3xl text-white">Hello!</p>
-                        <p className="text-white font-light">Terimakasih telah mengunjungi kami</p>
-                        <div className="flex flex-col gap-3 w-80 mt-8">
+                        <p className="font-bold text-3xl text-white">Register</p>
+                        <div className="flex flex-col gap-3 w-80 mt-6">
+                            <label className="input input-bordered flex items-center gap-2 h-10">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                                <input type="text" className="grow" placeholder="Name" />
+                            </label>
+                            <label className="input input-bordered flex items-center gap-2 h-10">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
+                                <input type="text" className="grow" placeholder="Username" />
+                            </label>
                             <label className="input input-bordered flex items-center gap-2 h-10">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70">
                                     <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
@@ -33,11 +40,11 @@ export default function Login() {
                                 </svg>
                                 <input type="password" className="grow" placeholder="password" />
                             </label>
-                            <button className="btn btn-primary btn-sm font-bold mt-4">Sign In</button>
+                            <button className="btn btn-primary btn-sm font-bold mt-4">Sign Up</button>
                         </div>
-                        <p className="text-white font-light mt-6">Belum memiliki akun ? {" "}
-                            <Link href="/register" className="text-blue-600 font-bold hover:text-slate-600">
-                                Register
+                        <p className="text-white font-light mt-6">Kembali ke halaman {" "}
+                            <Link href="/login" className="text-blue-600 font-bold hover:text-slate-600">
+                                Login
                             </Link>
                         </p>
                     </div>
